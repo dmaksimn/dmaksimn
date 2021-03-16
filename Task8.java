fce9e46a6332590dce31ab9b7eb0da9b52412a07
dmaksimn <80350425+dmaksimn@users.noreply.github.com>
2021-03-16 16:55:19 +0300

    Add files via upload

---
+++ Task8.java
@@ -0,0 +1,10 @@
+public class Task8 {
+    public static void main (String ... args) {
+        int array[] = {1, 2, 3, 4, 5, 3, 2, 1, 2, 6, 5, 4, 3, 3, 2, 1, 1, 3, 3, 3, 4, 2};
+        int sum = 0;
+        for (int i = 0; i < array[i]; i++) {
+            sum += array[i];
+            }
+        System.out.println("min number of roubles must have = " + sum);
+        }
+    }
